944b7e21d22a7f0a18cd0972c09abd9a28a3c8d2
Lars Martin Haugland <larmha@stud.ntnu.no>
2024-04-11 12:54:46 +0200

    fixed potential endless loop

--- src/ConversionTools/AddConverters.cs
+++ src/ConversionTools/AddConverters.cs
@@ -5,18 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 class AddConverters
 {
+    List<Converter> converters = null;
+
+
     public List<Converter> GetConverters()
     {
-        List<Converter> converters = new List<Converter>();
-        converters.Add(new iText7());
-        converters.Add(new GhostscriptConverter());
-        converters.Add(new LibreOfficeConverter());
-        converters.Add(new EmailConverter());
-        //Remove converters that are not supported on the current operating system
-        var currentOS = Environment.OSVersion.Platform.ToString();
-        converters.RemoveAll(c => c.SupportedOperatingSystems == null ||
-                                  !c.SupportedOperatingSystems.Contains(currentOS) ||
-                                  !c.DependeciesExists);
+        if (converters == null)
+        {
+            converters = new List<Converter>();
+            converters.Add(new iText7());
+            converters.Add(new GhostscriptConverter());
+            converters.Add(new LibreOfficeConverter());
+            converters.Add(new EmailConverter());
+            //Remove converters that are not supported on the current operating system
+            var currentOS = Environment.OSVersion.Platform.ToString();
+            converters.RemoveAll(c => c.SupportedOperatingSystems == null ||
+                                      !c.SupportedOperatingSystems.Contains(currentOS) ||
+                                      !c.DependeciesExists);
+        }
+        
         return converters;
     }
     private static AddConverters? instance;
