0381841b21d498b64adafa67e8022eac12fdd5f7
Lars Martin Haugland <larmha@stud.ntnu.no>
2024-05-09 12:34:38 +0200

    optimized usage of ICC file path

--- src/ConversionTools/Converters/iText7.cs
+++ src/ConversionTools/Converters/iText7.cs
@@ -31,7 +31,7 @@ namespace ConversionTools.Converters
     public class IText7 : Converter
     {
         private static readonly object pdfalock = new object();     //PDF-A uses a .icc file when converting, which can not be accessed by multiple threads at the same time
-
+        private static string ICCFilePath = GetICCFilePath();       //Path to the .icc file needed for PDF-A conversion
         public IText7()
         {
             Name = "iText7";
@@ -274,7 +274,7 @@ namespace ConversionTools.Converters
                 // Initialize PdfOutputIntent object
                 lock (pdfalock)
                 {
-                    using (FileStream iccFileStream = new FileStream(GetICCFilePath(), FileMode.Open))
+                    using (FileStream iccFileStream = new FileStream(ICCFilePath, FileMode.Open))
                     {
                         outputIntent = new PdfOutputIntent("Custom", "", "https://www.color.org", "sRGB IEC61966-2.1", iccFileStream);
                     }
@@ -784,20 +784,5 @@ namespace ConversionTools.Converters
             .Add("fmt/1910", PdfAConformanceLevel.PDF_A_4)
             .Add("fmt/1911", PdfAConformanceLevel.PDF_A_4E)
             .Add("fmt/1912", PdfAConformanceLevel.PDF_A_4F);
-        
-        /*static readonly public Dictionary<String, PdfAConformanceLevel> PronomToPdfAConformanceLevel = new Dictionary<string, PdfAConformanceLevel>()
-        {
-            {"fmt/95",  PdfAConformanceLevel.PDF_A_1A },
-            {"fmt/354", PdfAConformanceLevel.PDF_A_1B },
-            {"fmt/476", PdfAConformanceLevel.PDF_A_2A },
-            {"fmt/477", PdfAConformanceLevel.PDF_A_2B },
-            {"fmt/478", PdfAConformanceLevel.PDF_A_2U },
-            {"fmt/479", PdfAConformanceLevel.PDF_A_3A },
-            {"fmt/480", PdfAConformanceLevel.PDF_A_3B },
-            {"fmt/481", PdfAConformanceLevel.PDF_A_3U },
-            {"fmt/1910", PdfAConformanceLevel.PDF_A_4 },
-            {"fmt/1911", PdfAConformanceLevel.PDF_A_4E},
-            {"fmt/1912", PdfAConformanceLevel.PDF_A_4F}
-        };*/
     }
 }
