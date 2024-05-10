de556149470afdb6f487a3dabcff4c36e0ef8e8a
Philip <philiasu@stud.ntnu.no>
2024-05-10 12:38:43 +0200

    added logger test

---
+++ file-converter-prog2900Tests/src/Logger.cs
@@ -0,0 +1,43 @@
+﻿using FileConverter.HelperClasses;
+using iText.Kernel.Colors;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.DirectoryServices.AccountManagement;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace file_converter_prog2900Tests.src
+{
+    [TestClass()]
+    public class LoggerTests
+    {
+        Logger logger = Logger.Instance;
+        [TestMethod()]
+        public void TestSetRequesterAndConverter()
+        {
+            // Arrange
+            var expectedRequesterAndConverter = GetExpectedRequesterAndConverter();
+            Debug.WriteLine("Expected Requester and Converter: " + expectedRequesterAndConverter);
+            // Act
+            GlobalVariables.ParsedOptions.AcceptAll = true;
+            Logger.SetRequesterAndConverter();
+            // Assert
+            Assert.AreEqual(expectedRequesterAndConverter, Logger.JsonRoot.Requester);
+            Assert.AreEqual(expectedRequesterAndConverter, Logger.JsonRoot.Converter);
+            Debug.WriteLine("Actual Requester: " + Logger.JsonRoot.Requester);
+            Debug.WriteLine("Actual Converter: " + Logger.JsonRoot.Converter);
+        }
+
+        private static string GetExpectedRequesterAndConverter()
+        {
+            string user = Environment.UserName;
+            if (OperatingSystem.IsWindows())
+            {
+                user = UserPrincipal.Current.DisplayName;
+            }
+            return user;
+        }
+    }
+}
