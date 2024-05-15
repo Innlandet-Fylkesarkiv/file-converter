362083a7aed406a6f338e684e0df43026d96ad9f
aleksolh <aleksolh@stud.ntnu.no>
2024-05-15 13:25:01 +0200

    Added additional tests and moved logger to helperclasses tests

--- file-converter-prog2900Tests/src/HelperClasses.cs
+++ file-converter-prog2900Tests/src/HelperClasses.cs
@@ -1,13 +1,15 @@
 ﻿using FileConverter.HelperClasses;
 using FileConverter.Managers;
 using FileConverter.Siegfried;
+using System.Diagnostics;
+using System.DirectoryServices.AccountManagement;
 using SF = FileConverter.Siegfried;
 
 
 namespace FileConverter.HelperClasses.Tests
 {
     [TestClass()]
-    public class HelperClassesTests
+    public class FileInfo2Tests
     {
 
         [ClassInitialize]
@@ -61,6 +63,141 @@ namespace FileConverter.HelperClasses.Tests
 
             // Cleanup
             File.Delete(newFileName); // Delete the renamed file
+        }
+
+        [TestMethod]
+        public void TestFileInfo2ConstructorWithPathAndFileInfo()
+        {
+            // Arrange
+            var siegfriedFile = new SiegfriedFile
+            {
+                filesize = 100,
+                filename = "test.txt",
+                hash = "hash",
+                matches = new SiegfriedMatches[] { new SiegfriedMatches { id = "id", format = "format", mime = "mime" } }
+            };
+            FileInfo2 originalFileInfo = new FileInfo2(siegfriedFile);
+            FileInfo2 fileInfoToCopy = new FileInfo2("test.txt", originalFileInfo);
+
+            // Assert
+            Assert.AreEqual(originalFileInfo.FilePath, fileInfoToCopy.FilePath);
+            Assert.AreEqual(originalFileInfo.OriginalFilePath, fileInfoToCopy.OriginalFilePath);
+            Assert.AreEqual(originalFileInfo.OriginalPronom, fileInfoToCopy.OriginalPronom);
+            Assert.AreEqual(originalFileInfo.OriginalChecksum, fileInfoToCopy.OriginalChecksum);
+            Assert.AreEqual(originalFileInfo.OriginalFormatName, fileInfoToCopy.OriginalFormatName);
+            Assert.AreEqual(originalFileInfo.OriginalMime, fileInfoToCopy.OriginalMime);
+            Assert.AreEqual(originalFileInfo.OriginalSize, fileInfoToCopy.OriginalSize);
+            Assert.AreEqual(originalFileInfo.Parent, fileInfoToCopy.Parent);
+        }
+
+        [TestMethod]
+        public void TestFileInfo2ConstructorWithSiegfriedFile()
+        {
+            // Arrange
+            var siegfriedFile = new SiegfriedFile
+            {
+                filesize = 100,
+                filename = "test.txt",
+                hash = "hash",
+                matches = new SiegfriedMatches[] { new SiegfriedMatches { id = "id", format = "format", mime = "mime" } }
+            };
+
+            // Act
+            FileInfo2 fileInfo = new FileInfo2(siegfriedFile);
+
+            // Assert
+            Assert.AreEqual(siegfriedFile.filesize, fileInfo.OriginalSize);
+            Assert.AreEqual(siegfriedFile.filename, fileInfo.OriginalFilePath);
+            Assert.AreEqual(siegfriedFile.matches[0].id, fileInfo.OriginalPronom);
+            Assert.AreEqual(siegfriedFile.hash, fileInfo.OriginalChecksum);
+            Assert.AreEqual(siegfriedFile.matches[0].format, fileInfo.OriginalFormatName);
+            Assert.AreEqual(siegfriedFile.matches[0].mime, fileInfo.OriginalMime);
+        }
+
+        [TestMethod]
+        public void TestFileInfo2ConstructorWithFileToConvert()
+        {
+            // Arrange
+            string filePath = "test.txt";
+            var fileToConvert = new FileToConvert(filePath, Guid.NewGuid(), "targetPronom")
+            {
+                CurrentPronom = "currentPronom",
+                Route = new List<string> { "route1", "route2" },
+                IsModified = true,
+                addedDuringRun = true,
+            };
+
+            // Act
+            FileInfo2 fileInfo = new FileInfo2(fileToConvert);
+
+            // Assert
+            Assert.AreEqual(fileToConvert.FilePath, fileInfo.FilePath);
+            Assert.AreEqual(fileInfo.OriginalFilePath, Path.GetFileName(fileToConvert.FilePath));
+        }
+
+        [TestMethod]
+        public void TestUpdateSelfWithNonNullFileInfo()
+        {
+            // Arrange
+            var originalSiegfriedFile = new SiegfriedFile
+            {
+                filename = "test.txt",
+                filesize = 12345,
+                matches = new[] { new SiegfriedMatches { id = "pronom123", format = "FormatName", mime = "text/plain" } }
+            };
+
+            var newSiegfriedFile = new SiegfriedFile
+            {
+                filename = "test2.txt",
+                filesize = 54321,
+                matches = new[] { new SiegfriedMatches { id = "pronom456", format = "NewFormatName", mime = "text/csv" } }
+            };
+
+            // Act
+            var originalFileInfo = new FileInfo2(originalSiegfriedFile);
+            var newFileInfo = new FileInfo2(newSiegfriedFile);
+
+            // Act
+            originalFileInfo.UpdateSelf(newFileInfo);
+
+            // Assert
+            Assert.AreEqual(newFileInfo.OriginalPronom, originalFileInfo.NewPronom);
+            Assert.AreEqual(newFileInfo.OriginalFormatName, originalFileInfo.NewFormatName);
+            Assert.AreEqual(newFileInfo.OriginalMime, originalFileInfo.NewMime);
+            Assert.AreEqual(newFileInfo.OriginalSize, originalFileInfo.NewSize);
+            Assert.AreEqual(newFileInfo.OriginalChecksum, originalFileInfo.NewChecksum);
+        }
+    }
+
+
+    [TestClass()]
+    public class LoggerTests
+    {
+        private readonly Logger logger = Logger.Instance;
+        [TestMethod()]
+        public void TestSetRequesterAndConverter()
+        {
+            // Arrange
+            var expectedRequesterAndConverter = GetExpectedRequesterAndConverter();
+            Debug.WriteLine("Expected Requester and Converter: " + expectedRequesterAndConverter);
+            // Act
+            GlobalVariables.ParsedOptions.AcceptAll = true;
+            Logger.SetRequesterAndConverter();
+            // Assert
+            Assert.AreEqual(expectedRequesterAndConverter, Logger.JsonRoot.Requester);
+            Assert.AreEqual(expectedRequesterAndConverter, Logger.JsonRoot.Converter);
+            Debug.WriteLine("Actual Requester: " + Logger.JsonRoot.Requester);
+            Debug.WriteLine("Actual Converter: " + Logger.JsonRoot.Converter);
+        }
+
+        private static string GetExpectedRequesterAndConverter()
+        {
+            string user = Environment.UserName;
+            if (OperatingSystem.IsWindows())
+            {
+                user = UserPrincipal.Current.DisplayName;
+            }
+            return user;
         }
     }
 }
